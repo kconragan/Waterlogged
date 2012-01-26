caeaa2719bfcd06cdd3029d843c91e2a19198138
Kai Conragan <kai@conragan.com>
2012-01-25 19:51:32 -0800

    Moved to swig for templating language, cleaned up files accordingly

--- app.js
+++ app.js
@@ -4,15 +4,23 @@
  */
 
 var express = require('express')
+  , swig = require('swig')
   , routes = require('./routes')
+  , people
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
 app.configure(function(){
+  swig.init({
+    root: __dirname + '/views',
+    allowErrors: true // allows errors to be thrown and caught by express
+  });
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.register('.html', swig);
+  app.set('view engine', 'swig');
+  app.set('view options', { layout: false });
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -29,7 +37,9 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
+app.get('/', function (req, res) {
+    res.render('index.html', { foo: 'bar' });
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
