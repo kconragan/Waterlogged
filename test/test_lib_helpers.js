119125c3bd74ddb5ad45e21190745cfa20a97f61
Tyler Pirtle <rtp@google.com>
2012-04-19 13:21:29 -0700

    Add more tests, sort lib/helpers things, a little cleanup.

--- test/test_lib_helpers.js
+++ test/test_lib_helpers.js
@@ -7,17 +7,31 @@ var Q = require('q');
 var h = require('../lib/helpers.js');
 
 describe('Helper functions', function() {
+  describe('timeDelta function', function() {
+    it('should take two JavaScript date objects and return a valid delta',
+       function() {
+         // FORMAT: YYYY,MM,DD,H,HH
+         var date1 = moment('1 January 1970 00:00:00');
+         var date2 = moment('1 January 1980 00:00:00');
 
-  describe('compareTimeDelta function', function() {
-    it('should take two JavaScript date objects and return the delta', function() {
-      // FORMAT: YYYY,MM,DD,H,HH
-      var date1 = moment('1 January 1970 00:00:00');
-      var date2 = moment('1 January 1980 00:00:00');
+         // should be 315532800000
+         var delta = h.timeDelta(date2, date1);
+         expect(delta).to.be.a('number');
+         expect(delta).to.equal(315532800000);
 
-      var delta = h.compareTimeDelta(date1, date2); // should be -315532800000 
-      expect(delta).to.be.a('number');
-      expect(delta).to.equal(-315532800000);
+         // should be 0
+         date1 = moment('1 January 2012 00:00:00');
+         date2 = moment('1 January 2012 00:00:00');
+         delta = h.timeDelta(date2, date1);
+         expect(delta).to.be.a('number');
+         expect(delta).to.equal(0)
 
+         // # of millis in a day
+         date1 = moment('1 January 2012 00:00:00');
+         date2 = moment('2 January 2012 00:00:00');
+         delta = h.timeDelta(date2, date1);
+         expect(delta).to.be.a('number');
+         expect(delta).to.equal(60 * 60 * 24 * 1000)
     });
   });
 });
